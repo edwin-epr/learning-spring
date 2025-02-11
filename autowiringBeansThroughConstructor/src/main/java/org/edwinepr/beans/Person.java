436995d15f59aec5f26a23ebb180b554c91e46a5
Edwin Enrique Perez Rodriguez <edwin.epr.219@gmail.com>
2025-02-10 23:45:20 -0600

    Update course progress
    Add autowiring through constructor parameter section.

---
+++ autowiringBeansThroughConstructor/src/main/java/org/edwinepr/beans/Person.java
@@ -0,0 +1,18 @@
+package org.edwinepr.beans;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class Person {
+    private String name = "Marianne";
+
+    private final Parrot parrot;
+
+    @Autowired
+    public Person(Parrot parrot) {
+        this.parrot = parrot;
+    }
+}
