5911a7ec8bbb5d8e30ae67cabb9225824ee66fb5
Edwin Enrique Perez Rodriguez <edwin.epr.219@gmail.com>
2025-02-15 19:00:06 -0600

    Update learning spring
    Focusing on object responsabilities with stereotype annotations.

--- commentTaskApp/src/main/java/org/edwinepr/repositories/DBCommentRepository.java
+++ commentTaskApp/src/main/java/org/edwinepr/repositories/DBCommentRepository.java
@@ -1,9 +1,9 @@
 package org.edwinepr.repositories;
 
 import org.edwinepr.model.Comment;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public class DBCommentRepository implements CommentRepository {
     @Override
     public void storeComment(Comment comment) {
--- commentTaskApp/src/main/java/org/edwinepr/services/CommentService.java
+++ commentTaskApp/src/main/java/org/edwinepr/services/CommentService.java
@@ -5,9 +5,9 @@ import org.edwinepr.proxies.CommentNotificationProxy;
 import org.edwinepr.repositories.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class CommentService {
     private final CommentRepository commentRepository;
 
