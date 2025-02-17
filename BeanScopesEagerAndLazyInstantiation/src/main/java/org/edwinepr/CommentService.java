db95340aaba500a350c7f13c53c595f739e972f8
Edwin Enrique Perez Rodriguez <edwin.epr.219@gmail.com>
2025-02-16 19:52:17 -0600

    Update learning spring
    Using prototype-scope with bean annotation.

--- BeanScopesEagerAndLazyInstantiation/src/main/java/org/edwinepr/CommentService.java
+++ BeanScopesEagerAndLazyInstantiation/src/main/java/org/edwinepr/CommentService.java
@@ -1,12 +1,7 @@
 package org.edwinepr;
 
 import lombok.Data;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Service;
 
-@Service
-@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 @Data
 public class CommentService {
 }
--- BeanScopesEagerAndLazyInstantiation/src/main/java/org/edwinepr/ProjectConfig.java
+++ BeanScopesEagerAndLazyInstantiation/src/main/java/org/edwinepr/ProjectConfig.java
@@ -1,9 +1,16 @@
 package org.edwinepr;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
-@ComponentScan(basePackages = {"org.edwinepr"})
 public class ProjectConfig {
+
+    @Bean
+    @Scope(BeanDefinition.SCOPE_PROTOTYPE)
+    public CommentService commentService() {
+        return new CommentService();
+    }
 }
