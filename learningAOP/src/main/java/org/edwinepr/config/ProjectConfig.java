989b593d4e4aa3db7d2ba034c58d500e7f0bf8c2
Edwin Enrique Perez Rodriguez <edwin.epr.219@gmail.com>
2025-02-17 22:38:02 -0600

    Update learning spring
    Implementing a simple aspect.

--- learningAOP/src/main/java/org/edwinepr/config/ProjectConfig.java
+++ learningAOP/src/main/java/org/edwinepr/config/ProjectConfig.java
@@ -1,9 +1,18 @@
 package org.edwinepr.config;
 
+import org.edwinepr.aspects.LoggingAspect;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@EnableAspectJAutoProxy
 @ComponentScan(basePackages = {"org.edwinepr.services"})
 public class ProjectConfig {
+
+    @Bean
+    public LoggingAspect loggingAspect() {
+        return new LoggingAspect();
+    }
 }
