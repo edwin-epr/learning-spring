b08ae165036008c508bca904ab55e68762d754ef
Edwin Enrique Perez Rodriguez <edwin.epr.219@gmail.com>
2025-02-24 00:00:45 -0600

    Update learning spring
    First web app with spring boot. Create a spring boot project from
    zero.

---
+++ SpringBootMagic/src/main/java/org/edwinepr/springbootmagic/SpringBootMagicApplication.java
@@ -0,0 +1,13 @@
+package org.edwinepr.springbootmagic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMagicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMagicApplication.class, args);
+    }
+
+}
