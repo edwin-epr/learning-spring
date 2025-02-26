986d2c4c35a188fa1b3ddf2cb45949f6e15091e1
Edwin Enrique Perez Rodriguez <edwin.epr.219@gmail.com>
2025-02-25 23:05:45 -0600

    Update learning spring
    Implementing a web app with spring mvc.

--- SpringBootMagic/src/main/java/org/edwinepr/springbootmagic/main/SpringBootMagicApplication.java
+++ SpringBootMagic/src/main/java/org/edwinepr/springbootmagic/main/SpringBootMagicApplication.java
@@ -1,4 +1,4 @@
-package org.edwinepr.springbootmagic;
+package org.edwinepr.springbootmagic.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- SpringBootMagic/src/test/java/org/edwinepr/springbootmagic/SpringBootMagicApplicationTests.java
+++ SpringBootMagic/src/test/java/org/edwinepr/springbootmagic/SpringBootMagicApplicationTests.java
@@ -1,13 +1,28 @@
 package org.edwinepr.springbootmagic;
 
+import org.edwinepr.springbootmagic.main.SpringBootMagicApplication;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = SpringBootMagicApplication.class)
+@AutoConfigureMockMvc
 class SpringBootMagicApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    @DisplayName("Test that /home page can be successfully called.")
+    void testPageRequestAndContent() throws Exception {
+        mockMvc.perform(get("/home.html"))
+                .andExpect(status().isOk());
     }
 
 }
