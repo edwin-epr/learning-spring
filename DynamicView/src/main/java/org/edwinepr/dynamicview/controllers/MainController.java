135ee3b82bafbbb070444b3b192bca77eb101629
Edwin Enrique Perez Rodriguez <edwin.epr.219@gmail.com>
2025-03-01 17:49:56 -0600

    Update learning spring
    Using path variables to send data from client to server

--- DynamicView/src/main/java/org/edwinepr/dynamicview/controllers/MainController.java
+++ DynamicView/src/main/java/org/edwinepr/dynamicview/controllers/MainController.java
@@ -2,19 +2,18 @@ package org.edwinepr.dynamicview.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-    @RequestMapping("/home")
+    @RequestMapping("/home/{color}")
     public String home(
-            @RequestParam(required = false) String name,
-            @RequestParam(required = false) String color,
+            @PathVariable String color,
             Model page) {
-        page.addAttribute("username", name);
+        page.addAttribute("username", "Mariana");
         page.addAttribute("color", color);
         return "home";
     }
