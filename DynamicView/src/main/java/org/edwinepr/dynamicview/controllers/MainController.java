0d853987847490bc1c52489984d8556a6650ee00
Edwin Enrique Perez Rodriguez <edwin.epr.219@gmail.com>
2025-03-01 17:21:53 -0600

    Update learning spring
    Using request parameters to send data from client to server part 1

--- DynamicView/src/main/java/org/edwinepr/dynamicview/controllers/MainController.java
+++ DynamicView/src/main/java/org/edwinepr/dynamicview/controllers/MainController.java
@@ -3,14 +3,18 @@ package org.edwinepr.dynamicview.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/home")
-    public String home(Model page) {
+    @RequestMapping("/home")
+    public String home(
+            @RequestParam String color,
+            Model page) {
         page.addAttribute("username", "Mariana");
-        page.addAttribute("color", "blue");
+        page.addAttribute("color", color);
         return "home";
     }
 
