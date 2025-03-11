420132557580245e0bdb1901d4279385239e63cb
Edwin Enrique Perez Rodriguez <edwin.epr.219@gmail.com>
2025-03-10 22:34:20 -0600

    Update learning spring
    Creation of ImplRestService project. Implementing a REST endpoint.

---
+++ ImplRestService/src/main/java/org/edwinepr/implrestservice/ImplRestServiceApplication.java
@@ -0,0 +1,13 @@
+package org.edwinepr.implrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplRestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImplRestServiceApplication.class, args);
+    }
+
+}
---
+++ ImplRestService/src/test/java/org/edwinepr/implrestservice/ImplRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.edwinepr.implrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplRestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
