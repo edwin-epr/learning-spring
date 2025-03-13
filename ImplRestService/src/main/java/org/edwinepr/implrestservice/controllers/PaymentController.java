9adb66cd399d1641689c805c7637c818c72a132c
Edwin Enrique Perez Rodriguez <edwin.epr.219@gmail.com>
2025-03-12 23:08:44 -0600

    Update learning spring
    Using a request body to get data from the client.

--- ImplRestService/src/main/java/org/edwinepr/implrestservice/controllers/PaymentController.java
+++ ImplRestService/src/main/java/org/edwinepr/implrestservice/controllers/PaymentController.java
@@ -6,18 +6,21 @@ import org.edwinepr.implrestservice.service.PaymentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
-@RequiredArgsConstructor
 public class PaymentController {
 
-    private final PaymentService paymentService;
+    private static Logger LOGGER = Logger.getLogger(PaymentController.class.getName());
 
     @PostMapping(path = "/payment")
-    public ResponseEntity<?> makePayment() {
-         PaymentDetails paymentDetails = paymentService.processPayment();
-
+    public ResponseEntity<PaymentDetails> makePayment(
+            @RequestBody PaymentDetails paymentDetails
+    ) {
+        LOGGER.info("Received payment for " + paymentDetails.getAmount());
          return ResponseEntity
                 .status(HttpStatus.ACCEPTED)
                 .body(paymentDetails);
